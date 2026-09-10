6c7f8270fa30142142bd42248851f8cd47d556e0
Daveroum345 <daveloua49@gmail.com>
2026-09-10 00:07:15 +0000

    Complete Exercise 4

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,30 @@
-using System;
+using System.Collections.Generic;
 
-class Program
+List<int> numbers = new List<int>();
+
+Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+int number = -1;
+
+while (number != 0)
 {
-    static void Main(string[] args)
+    Console.Write("Enter number: ");
+    number = int.Parse(Console.ReadLine());
+
+    if (number != 0)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        numbers.Add(number);
     }
 }
+
+int sum = 0;
+
+foreach (int currentNumber in numbers)
+{
+    sum += currentNumber;
+}
+
+Console.WriteLine($"The sum is: {sum}");
+double average = (double)sum / numbers.Count;
+
+Console.WriteLine($"The average is: {average}");
